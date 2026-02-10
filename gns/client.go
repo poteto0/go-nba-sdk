79c59e987962060251a42ae39723b29675086d95
poteto0 <mahiro.fukuda.aries@gmail.com>
2026-02-10 22:14:09 +0900

    fix: pre-release bug fix. refs: #9

--- gns/client.go
+++ gns/client.go
@@ -1,8 +1,6 @@
 package gns
 
 import (
-	"time"
-
 	"github.com/poteto0/go-nba-sdk/api"
 	"github.com/poteto0/go-nba-sdk/namespace"
 	"github.com/poteto0/go-nba-sdk/types"
@@ -13,19 +11,7 @@ type Client struct {
 	Live  namespace.ILiveNamespace
 }
 
-var DefaultConfig = &types.GnsConfig{
-	Timeout: 10 * time.Second,
-}
-
 func NewClient(config *types.GnsConfig) *Client {
-	if config == nil {
-		config = DefaultConfig
-	}
-
-	if config.Timeout == 0 {
-		config.Timeout = DefaultConfig.Timeout
-	}
-
 	provider := api.NewProvider(
 		config,
 	)
--- internal/http_client.go
+++ internal/http_client.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/poteto0/go-nba-sdk/constants"
 	"github.com/poteto0/go-nba-sdk/types"
@@ -11,11 +12,23 @@ type HttpClient struct {
 	client *http.Client
 }
 
+var DefaultConfig = &types.GnsConfig{
+	Timeout: 10 * time.Second,
+}
+
 type IHttpClient interface {
 	Get(path string, headers *http.Header) (*http.Response, error)
 }
 
 func NewHttpClient(config *types.GnsConfig) IHttpClient {
+	if config == nil {
+		config = DefaultConfig
+	}
+
+	if config.Timeout == 0 {
+		config.Timeout = DefaultConfig.Timeout
+	}
+
 	return &HttpClient{
 		client: &http.Client{
 			Timeout: config.Timeout,
