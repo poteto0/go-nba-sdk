39890a43d14345831b51499d07e756220deb1e78
poteto0 <mahiro.fukuda.aries@gmail.com>
2026-05-14 22:29:40 +0900

    feat: implement exponential backoff mechanism

---
+++ internal/backoff.go
@@ -0,0 +1,63 @@
+package internal
+
+import (
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/poteto0/go-nba-sdk/types"
+)
+
+type BackoffManager struct {
+	config    types.BackoffConfig
+	retries   int
+	lastDelay float64
+	lock      sync.Mutex
+}
+
+func NewBackoffManager(config types.BackoffConfig) *BackoffManager {
+	return &BackoffManager{
+		config:    config,
+		retries:   0,
+		lastDelay: 0,
+	}
+}
+
+func (b *BackoffManager) Backoff() error {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	if b.retries >= b.config.MaxRetries {
+		return types.ErrOverMaxRetries
+	}
+
+	currentDelay := b.calculateBackOffDelay()
+
+	currentDelay = math.Max(currentDelay, b.config.InitialDelayMs)
+	currentDelay = math.Min(currentDelay, b.config.MaxDelayMs)
+
+	b.retries++
+	b.lastDelay = currentDelay
+
+	time.Sleep(time.Duration(currentDelay) * time.Millisecond)
+
+	return nil
+}
+
+func (b *BackoffManager) calculateBackOffDelay() float64 {
+	switch b.config.Mode {
+	case "exponential":
+		return b.calculateExponentialBackOffDelay()
+	default:
+		return b.config.InitialDelayMs
+	}
+}
+
+func (b *BackoffManager) calculateExponentialBackOffDelay() float64 {
+	if b.retries == 0 {
+		return float64(0)
+	}
+
+	return math.Min(b.lastDelay+(rand.Float64()-0.5)*b.lastDelay, b.config.MaxDelayMs)
+}
--- types/config.go
+++ types/config.go
@@ -2,10 +2,19 @@ package types
 
 import "time"
 
+type BackoffConfig struct {
+	Mode           string
+	MaxRetries     int
+	InitialDelayMs float64
+	MaxDelayMs     float64
+}
+
 type GnsConfig struct {
 	// default is 10s
 	Timeout time.Duration
 
 	// default is empty string
 	ProxyUrl string
+
+	Backoff BackoffConfig
 }
